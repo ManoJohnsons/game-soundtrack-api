ddf3587e066258a7513b029616104cc6a1226961
John Paulo <johnpaulo0401@gmail.com>
2025-09-25 13:37:39 -0300

    feat(user): create endpoints to update and delete users

--- src/main/java/io/github/manojohnsons/gamesoundtracksapi/user/UserController.java
+++ src/main/java/io/github/manojohnsons/gamesoundtracksapi/user/UserController.java
@@ -4,20 +4,14 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import io.github.manojohnsons.gamesoundtracksapi.user.dtos.UserRequestDTO;
 import io.github.manojohnsons.gamesoundtracksapi.user.dtos.UserResponseDTO;
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 
 @RestController
 @RequestMapping("/users")
@@ -41,13 +35,25 @@ public class UserController {
 
         return ResponseEntity.ok(allUsers);
     }
-    
+
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
         UserResponseDTO userFetched = service.getUserById(id);
 
         return ResponseEntity.ok(userFetched);
     }
-    
 
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id,
+            @RequestBody @Valid UserRequestDTO userRequestDTO) {
+        UserResponseDTO userToUpdate = service.updateUserById(id, userRequestDTO);
+
+        return ResponseEntity.ok(userToUpdate);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        service.deleteUserById(id);
+    }
 }
--- src/main/java/io/github/manojohnsons/gamesoundtracksapi/user/UserService.java
+++ src/main/java/io/github/manojohnsons/gamesoundtracksapi/user/UserService.java
@@ -12,13 +12,13 @@ import io.github.manojohnsons.gamesoundtracksapi.user.dtos.UserResponseDTO;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository repository;
 
     @Transactional
     public UserResponseDTO insertUser(UserRequestDTO userRequestDTO) {
-        //TODO: Add password encryption.
+        // TODO: Add password encryption.
         User newUser = new User(userRequestDTO.getUsername(), userRequestDTO.getPassword());
         User userSaved = repository.save(newUser);
 
@@ -37,5 +37,24 @@ public class UserService {
         List<User> allUsers = repository.findAll();
 
         return allUsers.stream().map(UserResponseDTO::new).toList();
+    }
+
+    @Transactional
+    public UserResponseDTO updateUserById(Long id, UserRequestDTO userRequestDTO) {
+        User userToUpdate = repository.findById(id).orElseThrow(NoSuchElementException::new);
+        userToUpdate.setUsername(userRequestDTO.getUsername());
+        userToUpdate.setPassword(userRequestDTO.getPassword()); // TODO: Encrypt new password
+        User userUpdated = repository.save(userToUpdate);
+
+        return new UserResponseDTO(userUpdated);
+    }
+
+    @Transactional
+    public void deleteUserById(Long id) {
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Usuario não encontrado com o ID: " + id);
+        }
+
+        repository.deleteById(id);
     }
 }
